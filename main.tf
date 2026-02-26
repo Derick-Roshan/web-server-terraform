216ce92f0278f0411f974bf04f765e21cdde3a4e
Roshan <roshansmesh@gmail.com>
2026-02-26 08:04:48 +0530

    second commit

--- main.tf
+++ main.tf
@@ -78,7 +78,8 @@ resource "aws_instance" "web-server" {
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name               = "spider.pem"
+  key_name               = "spider"
+
 
   user_data = <<-EOF
     #!/bin/bash
